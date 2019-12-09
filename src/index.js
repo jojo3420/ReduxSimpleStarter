02b5cd161903cbff1b3cc32c8f72d53e00be7eb7
dev_pjh-at-670977422354 <jjjhhhvvv@gmail.com>
2019-12-09 11:12:54 +0900

    챕터5 - 프로젝트 소스 clear

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
-import { applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import ReduxPromise from "redux-promise";  // 리덕스-미들웨어
 
-import App from "./components/app";
-import reducers from "./reducers";
-
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const App = () => <div>App</div>;
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App/>
-  </Provider>,
+    <App/>,
     document.querySelector(".container"));
